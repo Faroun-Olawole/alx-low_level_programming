d386659a9b2e87a21378abdf9bc4f9a1649c1f5a
Faroun-Olawole <farounolawole7@gmail.com>
2023-10-02 14:22:50 -0700

    Updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,6 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+
 
 /**
  * read_textfile - Reads and prints a text file to the POSIX standard output.
@@ -12,41 +11,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, bytes_read, bytes_written;
-    char *buffer;
+	char *buffer;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t c;
 
-    if (filename == NULL)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	c = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, c);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
-
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
-
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
-
-    close(fd);
-    free(buffer);
-    return (bytes_written);
+	free(buffer);
+	close(fd);
+	return (w);
 }
-
